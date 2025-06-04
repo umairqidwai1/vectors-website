dfd964f33d7f553d36982227b0b65680933f8800
umairqidwai1 <134027101+umairqidwai1@users.noreply.github.com>
2025-06-04 19:14:00 -0400

    seo sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://vectors.build',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'weekly',
+  priority: 0.7,
+}
